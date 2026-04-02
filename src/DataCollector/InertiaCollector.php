137453ed60154e4e1ac1f43700c59c14e7ae74b4
Lovly <youssef@gameboost.com>
2026-04-02 19:15:11 +0200

    Fix Inertia tab truncating nested arrays by using deep formatting
    The parent TemplateCollector::addTemplate() calls formatVar with
    $deep=false, limiting array depth to 1. Override addTemplate in
    InertiaCollector to use the default $deep=true so nested props
    (auth, session, languages, etc.) can be fully expanded in the UI.

--- src/DataCollector/InertiaCollector.php
+++ src/DataCollector/InertiaCollector.php
@@ -12,6 +12,41 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class InertiaCollector extends TemplateCollector
 {
+    public function addTemplate(string $name, array $data, ?string $type, ?string $path): void
+    {
+        $hash = $type . $path . $name . ($this->collect_data ? implode(array_keys($data)) : '');
+
+        if ($this->collect_data === 'keys') {
+            $params = array_keys($data);
+        } elseif ($this->collect_data) {
+            $params = array_map(
+                fn($value) => $this->getDataFormatter()->formatVar($value),
+                $data,
+            );
+        } else {
+            $params = [];
+        }
+
+        $template = [
+            'name' => $name,
+            'param_count' => $this->collect_data ? count($params) : null,
+            'params' => $params,
+            'start' => microtime(true),
+            'type' => $type,
+            'hash' => $hash,
+        ];
+
+        if ($path && $this->getXdebugLinkTemplate()) {
+            $template['xdebug_link'] = $this->getXdebugLink($path);
+        }
+
+        if ($this->hasTimeDataCollector()) {
+            $this->addTimeMeasure($name, $template['start']);
+        }
+
+        $this->templates[] = $template;
+    }
+
     public function addFromView(\Illuminate\View\View $view): void
     {
         $data = $view->getData();
